1dcbbbe41d9401c0d3a4cce2877808c034c820bb
hwang <2484760160@qq.com>
2023-11-11 22:55:12 +0800

    up

--- 功能测试/douyin_comment.py
+++
@@ -1,8 +0,0 @@
-import uiautomator2 as u2
-d = u2.connect("emulator-5554")
-# 抖音评论
-
-d.click(0.9, 0.765)
-d(resourceId="com.ss.android.ugc.aweme:id/ft3").click()
-d.send_keys("1", clear=True)
-d(resourceId="com.ss.android.ugc.aweme:id/i24").click()
